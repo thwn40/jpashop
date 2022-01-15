ede88a9654a7af9ee99e7356f53704aab0fe1de9
thwn40 <62760166+thwn40@users.noreply.github.com>
2022-01-15 20:34:32 +0900

    20220115

--- jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exeption.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -24,6 +25,19 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<Category>();
+
+    public void  addStock(int quantity){
+        this.stockQuantity +=quantity;
+    }
+
+    public void removeStock(int quantity){
+        int restStock = this.stockQuantity - quantity;
+        if(restStock <0){
+            throw new NotEnoughStockException("need more stock");
+
+        }
+        this.stockQuantity = restStock;
+    }
 
 
 }
