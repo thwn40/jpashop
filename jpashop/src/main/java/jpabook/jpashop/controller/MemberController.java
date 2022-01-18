413f94935c535fc10a813f17d2f8bd07759ca2d2
thwn40 <62760166+thwn40@users.noreply.github.com>
2022-01-18 19:42:53 +0900

    20220118

--- jpashop/src/main/java/jpabook/jpashop/controller/MemberController.java
+++ jpashop/src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -7,10 +7,12 @@ import jpabook.jpashop.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -24,7 +26,11 @@ private final MemberService memberService;
 }
 
 @PostMapping("/members/new")
-    public String create(@Valid MemberForm form){
+    public String create(@Valid MemberForm form, BindingResult result){
+    if(result.hasErrors()){
+        return "members/createMemberForm";
+    }
+
     Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
 
     Member member = new Member();
@@ -34,5 +40,12 @@ private final MemberService memberService;
     memberService.join(member);
     return "redirect:/";
 
+}
+
+@GetMapping("/members")
+    public String list(Model model){
+    List<Member> members = memberService.findMembers();
+    model.addAttribute("members", memberService.findMembers());
+    return "members/memberList";
 }
 }
